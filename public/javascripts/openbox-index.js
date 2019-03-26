8cec1f3b36450fe6bf5a2771f8fd8f3e0ebb700d
weihaiming90 <weihaiming90@163.com>
2019-03-26 09:58:11 +0800

    update opnebox module  create openbox.js

--- public/javascripts/openbox-index.js
+++ public/javascripts/openbox-index.js
@@ -1,44 +1,9 @@
-
-const shadedom = $('.shade');
-const logindom = $('.login');
+import openbox from './openbox.js';
 const loginbuttondom = $('.loginbutton');
-
-const bwidth = $(document.body).outerWidth(true);
-const bheight = $(document.body).outerHeight(true);
-const cheight = $(window).height();
-console.log(cheight);
-
-const lwidth = logindom.width();
-const lheight = logindom.height();
-
-const ltop = (cheight-lheight)/2;
-const left = (bwidth-lwidth)/2;
-console.log(ltop,left);
-
-function openbox(){
-    shadedom.css({
-        'display':'block',
-        'width':bwidth+'px',
-        'height':bheight+'px'
-    })
-    logindom.css({
-        'display':'block',
-        'left':left,
-        'top':ltop,
-    })
-    // $('.' + className).click(function () {
-    //     close();
-    // });
-    shadedom.click(function () {
-        close();
-    });
-
-    //隐藏遮罩层和登录框
-    function close() {
-        shadedom.css('display', 'none');
-        logindom.css('display', 'none');
-    }
-}
-loginbuttondom.click(function(){
-    openbox();
+const registerbuttondom  = $('.registerbutton');
+loginbuttondom.click(function () {
+    openbox('login');
+});
+registerbuttondom.click(function () {
+    openbox('register');
 })
